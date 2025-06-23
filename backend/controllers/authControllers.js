a154a0609938f6d87aee38073ccaf52be67ba853
Abhi Passi <abhijit36254@gmail.com>
2025-06-23 23:02:53 +0530

    created route to load all the messages

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -149,6 +149,8 @@ exports.getMessages = async (req, res) => {
   try {
     const messages = await Message.find().sort({ createdAt: 1 });
     res.json(messages);
+
+    
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
   }
