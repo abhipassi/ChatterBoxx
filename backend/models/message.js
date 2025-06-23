b852605956747f669d033a6224a64b0793ef853c
Abhi Passi <abhijit36254@gmail.com>
2025-06-23 23:03:36 +0530

    updated message schema with sender and time stapms enabled

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,9 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: Date,
+    default: Date.now,
+  }
+});
 
-const MessageSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  text: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Message', MessageSchema);
+module.exports = mongoose.model('Message', messageSchema);
