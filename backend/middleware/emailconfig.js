24abc0088be45da5f82b4941c6e2172f399e811c
Abhi Passi <abhijit36254@gmail.com>
2025-07-04 13:26:25 +0530

    updated

--- backend/middleware/emailconfig.js
+++ backend/middleware/emailconfig.js
@@ -1,8 +1,8 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer'
 import dotenv from 'dotenv';
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
+export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // true for 465, false for other ports
@@ -11,21 +11,3 @@ const transporter = nodemailer.createTransport({
     pass: process.env.ACCOUNT_PASS,
   },
 }); 
-
-module.exports = transporter
-// const sendEmail = async () => {
-//     try {
-//         const info = await transporter.sendMail({
-//     from: '"Chatterboc" <chatterboox436@gmail.com>',
-//     to: "abhijit36254@gmail.com",
-//     subject: "Hello ✔",
-//     text: "Hello world?", // plain‑text body
-//     html: "<b>Hello world?</b>", // HTML body
-//   });
-//   console.log("Message sent:", info.messageId);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// };
-
-// sendEmail()
